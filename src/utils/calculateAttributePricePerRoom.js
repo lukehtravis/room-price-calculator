166ea92aaf68b16eb1b06cb708ac6b1ce8dd99ad
lukehtravis <lukehtravis@gmail.com>
2024-01-05 18:49:33 -0800

    write tests and cleanup readme

--- src/utils/calculateAttributePricePerRoom.js
+++ src/utils/calculateAttributePricePerRoom.js
@@ -5,6 +5,10 @@ const calculateAttributePricePerRoom = (
   attributePercentage,
   totalRent,
 ) => {
+  // Check if totalAttributeUnits is 0 to avoid division by zero
+  if (totalAttributeUnits === 0) {
+    return 0;
+  }
   return roundNumber(
     (roomUnits / totalAttributeUnits) *
       ((attributePercentage / 100) * totalRent),
--- src/utils/calculateUnitTotals.js
+++
@@ -1,5 +0,0 @@
-const calculateUnitTotals = (arrayOfNumbers) => {
-  return arrayOfNumbers.reduce((acc, curr) => acc + +curr.value, 0);
-};
-
-export default calculateUnitTotals;
